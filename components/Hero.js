11114d7e85b0d20cc0751491e9bf6436d325edd7
tasmoura <thiagoasmoura@gmail.com>
2021-07-05 17:35:53 -0300

    ajuste no hero

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,6 @@ function Hero() {
             <div className={heroStyles.herovid}>
                 <span className={heroStyles.sronly}>vídeo de apresentação: aposto comunicação, marketing com vida. Marketing muito além do habitual.</span>
             </div>
-            <h1><span>Conectar</span> pessoas, lugares e negócios através da <span>comunicação</span></h1>
         </section>
     )
 }
